0fd272e167f0837a51d2de98134ace01ae84f34c
Anesidora II <vickbelo17@gmail.com>
2023-10-24 18:24:24 -0300

    implementando camada lógica de usuários

--- src/interface/auth.interface.ts
+++ src/interface/auth.interface.ts
@@ -5,7 +5,6 @@ import { UserDTO } from "../dto/user.dto";
 export interface AuthInterface{
     login(credential: any): any;
     register(dto:UserDTO): Promise<UserEntity>;
-    update(id:number, dto:UserDTO):Promise<UserEntity>;   
     hashing(password:string):Promise<string>;
     unhashing(key: string, hash: string):Promise<boolean>;
 }
